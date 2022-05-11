6524fe5a817de1dc510a9db4db57a018bca52dbc
Nathan Lam <90929816+NatoNaCl@users.noreply.github.com>
2022-05-11 12:04:47 -0700

    Delete 8kyu directory

--- 8kyu/removeExclamationMarks.js
+++
@@ -1,9 +0,0 @@
-// Write function RemoveExclamationMarks which removes all exclamation marks from a given string.
-
-function removeExclamationMarks(s) {
-  return s.replace(/!/g, "");
-  // return s.replace(/!/gi, "");
-}
-
-//The /g flag is a global search.
-//The /gi flag is a case-insensitive search so upper or lower do not matter.
--- 8kyu/returningStrings.js
+++
@@ -1,9 +0,0 @@
-// Make a function that will return a greeting statement that uses an input;
-// your program should return, "Hello, <name> how are you doing today?".
-
-function greet(name) {
-  return `Hello, ${name} how are you doing today?`;
-}
-
-// Using an arrow function
-const greet = (name) => `Hello, ${name} how are you doing today?`;
