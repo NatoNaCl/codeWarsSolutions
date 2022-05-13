8c7360752646825392f893193079018d0fb8207b
Nathan Lam <nathanlam920@gmail.com>
2022-05-13 11:59:24 -0700

    Completed filterOutTheGeese exercise

---
+++ 8kyu/filterOutTheGeese.js
@@ -0,0 +1,48 @@
+/*
+Write a function that takes a list of strings as an argument and returns a
+filtered list containing the same elements but with the 'geese' removed.
+
+The geese are any strings in the following array, which is pre-populated in
+your solution:
+
+  ["African", "Roman Tufted", "Toulouse", "Pilgrim", "Steinbacher"]
+
+For example, if this array were passed as an argument:
+
+ ["Mallard", "Hook Bill", "African", "Crested", "Pilgrim", "Toulouse",
+ "Blue Swedish"]
+
+Your function would return the following array:
+
+["Mallard", "Hook Bill", "Crested", "Blue Swedish"]
+
+The elements in the returned array should be in the same order as in the
+initial array passed to your function, albeit with the 'geese' removed. Note
+that all of the strings will be in the same case as those provided, and some
+elements may be repeated.
+*/
+
+//P: pass in array of strings
+//R: return array of strings without geese from a given list
+//E:
+//Input: ["Mallard","Hook Bill","African","Crested","Pilgrim","Toulouse","Blue Swedish",]
+//Output: ["Mallard", "Hook Bill", "Crested", "Blue Swedish"]
+//P:
+//loop through birds array
+//loop through geese
+//conditional to compare bird to geese
+//remove bird if it matches a geese
+//return modified array
+
+function gooseFilter(birds) {
+  var geese = ["African", "Roman Tufted", "Toulouse", "Pilgrim", "Steinbacher"];
+  for (let i = 0; i < birds.length; i++) {
+    for (let j = 0; j < geese.length; j++) {
+      if (birds[i] === geese[j]) {
+        birds.splice(i, 1);
+        i--;
+      }
+    }
+  }
+  return birds;
+}
