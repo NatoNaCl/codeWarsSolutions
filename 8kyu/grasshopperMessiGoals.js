adf4ca567fffc8f90b697be4ec08b20c091e75e1
Nathan Lam <nathanlam920@gmail.com>
2022-05-16 12:12:30 -0700

    Completed grasshopperMessiGoals exercise

---
+++ 8kyu/grasshopperMessiGoals.js
@@ -0,0 +1,33 @@
+/*
+Messi's Goal Total
+
+Use variables to find the sum of the goals Messi scored in 3 competitions
+Information
+
+Messi goal scoring statistics:
+Competition 	Goals
+La Liga 	43
+Champions League 	10
+Copa del Rey 	5
+Task
+
+    Create these three variables and store the appropriate values using the table above:
+
+    laLigaGoals
+    championsLeagueGoals
+    copaDelReyGoals
+
+    Create a fourth variable named totalGoals that stores the sum of all of Messi's goals for this year.
+*/
+
+//inputs: Messi's goals from 3 competitions
+//outputs: Messi's total number of goals
+//plan:
+//declare 3 variables to store goals from each competition
+//declare a variable to sum all goals
+
+var laLigaGoals = 43;
+var championsLeagueGoals = 10;
+var copaDelReyGoals = 5;
+
+var totalGoals = laLigaGoals + championsLeagueGoals + copaDelReyGoals;
