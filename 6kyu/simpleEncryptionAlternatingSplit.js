5e076fef5425fed2b3a5dc7be2a24902fa573b78
Nathan Lam <nathanlam920@gmail.com>
2022-06-27 09:17:26 -0700

    Completed simpleEncryptionAlternatingSplit exercise

---
+++ 6kyu/simpleEncryptionAlternatingSplit.js
@@ -0,0 +1,71 @@
+/*
+Implement a pseudo-encryption algorithm which given a string S and an
+integer N concatenates all the odd-indexed characters of S with all the
+even-indexed characters of S, this process should be repeated N times.
+
+Examples:
+
+encrypt("012345", 1)  =>  "135024"
+encrypt("012345", 2)  =>  "135024"  ->  "304152"
+encrypt("012345", 3)  =>  "135024"  ->  "304152"  ->  "012345"
+
+encrypt("01234", 1)  =>  "13024"
+encrypt("01234", 2)  =>  "13024"  ->  "32104"
+encrypt("01234", 3)  =>  "13024"  ->  "32104"  ->  "20314"
+
+Together with the encryption function, you should also implement a
+decryption function which reverses the process.
+
+If the string S is an empty value or the integer N is not positive,
+return the first argument without changes.
+*/
+
+//Input: string and integer(number of iterations)
+//Output: string
+
+//encryption:
+//iterate over string
+//check for odd indexed characters
+//check for even indexed characters
+//concatenate n times
+//return string
+
+//decryption:
+//check if string is null or negative, return string
+//iterate over string
+//sort from smallest to largest
+//return string
+
+function encrypt(text, n) {
+  if (!text || n <= 0) return text;
+
+  while (n--) {
+    let concatenated = "";
+    for (let i = 1; i < text.length; i += 2) {
+      concatenated += text[i];
+    }
+    for (let i = 0; i < text.length; i += 2) {
+      concatenated += text[i];
+    }
+    text = concatenated;
+  }
+  return text;
+}
+
+function decrypt(encryptedText, n) {
+  if (!encryptedText || n <= 0) return encryptedText;
+
+  let newText = new Array(encryptedText.length);
+
+  while (n--) {
+    let j = 0;
+    for (let i = 1; i < newText.length; i += 2) {
+      newText[i] = encryptedText[j++];
+    }
+    for (let i = 0; i < newText.length; i += 2) {
+      newText[i] = encryptedText[j++];
+    }
+    encryptedText = newText.join("");
+  }
+  return encryptedText;
+}
